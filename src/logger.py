1f23a80ed53ba6f8871809673020b958b87652bd
Amandeepsinghn <amandeepsingh.kaillay@gmail.com>
2024-03-24 16:43:30 +0530

    logger and exception

---
+++ src/logger.py
@@ -0,0 +1,23 @@
+import logging 
+import sys 
+import os 
+from datetime import datetime
+from src.exception import CustomException
+
+
+LOG_File=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+logs_path=os.path.join(os.getcwd(),'logs',LOG_File)
+os.makedirs(logs_path,exist_ok=True)
+
+LOG_FILE_PATH=os.path.join(logs_path,LOG_File)
+
+
+
+logging.basicConfig(
+    level=logging.INFO,
+    filename=LOG_FILE_PATH,
+    format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s"
+)
+
+    
+    
